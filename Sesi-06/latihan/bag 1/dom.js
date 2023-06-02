25f5521112244831e193394d2f36d9f9bfa3d533
Fani Mulyana <fanimu7@gmail.com>
2023-06-02 21:06:45 +0700

    sesi 6 update

--- Sesi-06/latihan/bag 1/dom.js
+++ Sesi-06/latihan/bag 1/dom.js
@@ -1,1 +1,11 @@
+// document.write('Hello World');
+// document.write('<h2>Saya Sedang Belajar DOM</h2>');
 
+// mengakses objek head
+document.head;
+
+// mengakses objeck body
+document.body;
+
+// melihat jumlah huruf di judul pada objek title
+document.title.length;
